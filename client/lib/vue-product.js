0364161cdc53e877fc33075074893c19a7ba838a
ypnos <git@lanrules.de>
2019-01-13 23:24:29 +0100

    show popups as feedback to button press
    must be triggered by parent (method add_popup)

--- client/lib/vue-product.js
+++ client/lib/vue-product.js
@@ -1,3 +1,5 @@
+let uid = 0; // unique id for each product component
+
 Vue.component('product', {
 	template: `
 	<b-card
@@ -16,19 +18,38 @@ Vue.component('product', {
 			</span>
 		</div>
 		<b-button v-on:click="$emit('order')"
+			:id="buttonId"
 			class="buy mt-auto" block size="lg" variant="outline-success"
 		>
 			€ <strong>{{ format_money(product.prize) }}</strong>
 		</b-button>
+		<b-popover
+			v-for="n in numPopup" :key="n"
+			:target="buttonId" :show="true" triggers=""
+			:placement="['right', 'bottom', 'left', 'top'][(n-1) % 4]" :offset="((n-1)/4)*10"
+			:content="(n == 1 ? 'Gekauft!' : 'Nochmal gekauft!')">
+		</b-popover>
 	</b-card>
 	`,
 	props: {
 		product: {
 			required: true
-		},
+		}
+	},
+	data: function() {
+		uid++;
+		return {
+			buttonId: 'product_button_' + uid,
+			numPopup: 0
+		}
 	},
 	methods: {
-		format_money: value => Number.parseFloat(value).toFixed(2)
+		format_money: value => Number.parseFloat(value).toFixed(2),
+		add_popup: function () {
+			this.numPopup++;
+			let that = this;
+			window.setTimeout(() => that.numPopup = Math.max(that.numPopup - 1, 0), 3000);
+		}
 	}
 });
 
