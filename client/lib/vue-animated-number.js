e1cfbc9f7e906270aa30a62c88ffacc1895912f5
ypnos <git@lanrules.de>
2019-01-27 21:31:40 +0100

    code style/formatting

--- client/lib/vue-animated-number.js
+++ client/lib/vue-animated-number.js
@@ -1,53 +1,51 @@
 Vue.component('animated-number', {
-  template: '<span>{{ tweeningValue }}</span>',
-  props: {
-    value: {
-      type: Number,
-      required: true
-    },
-    decimals: {
-      type: Number,
-      default: 0
-    },
-    duration: {
-      type: Number,
-      default: 500
-    }
-  },
-  data: function () {
-    return {
-      tweeningValue: 0
-    }
-  },
-  watch: {
-    value: function (newValue, oldValue) {
-      this.tween(oldValue, newValue)
-    }
-  },
-  mounted: function () {
-    this.tween(0, this.value)
-  },
-  methods: {
-	dynamicDuration: function (distance) {
-		return this.duration * Math.log(Math.abs(distance) + 1);
+	template: '<span>{{ tweeningValue }}</span>',
+	props: {
+		value: {
+			type: Number,
+			required: true
+		},
+		decimals: {
+			type: Number,
+			default: 0
+		},
+		duration: {
+			type: Number,
+			default: 500
+		}
 	},
-    tween: function (startValue, endValue) {
-      var vm = this
-      function animate () {
-        if (TWEEN.update()) {
-          requestAnimationFrame(animate)
-        }
-      }
+	data: function () {
+		return {
+			tweeningValue: 0
+		}
+	},
+	watch: {
+		value: function (newValue, oldValue) {
+			this.tween(oldValue, newValue);
+		}
+	},
+	mounted: function () {
+		this.tweeningValue = this.value.toFixed(this.decimals);
+	},
+	methods: {
+		dynamicDuration: function (distance) {
+			return this.duration * Math.log(Math.abs(distance) + 1);
+		},
+		tween: function (start, end) {
+			var vm = this;
+			function animate () {
+				if (TWEEN.update()) {
+					requestAnimationFrame(animate);
+				}
+			}
 
-      new TWEEN.Tween({ tweeningValue: startValue })
-        .to({ tweeningValue: endValue }, this.dynamicDuration(endValue - startValue))
-        .onUpdate(function () {
-          vm.tweeningValue = this.tweeningValue.toFixed(vm.decimals)
-        })
-        .start()
-
-      animate()
-    }
-  }
-})
-
+			new TWEEN.Tween({ tweeningValue: start })
+				.to({ tweeningValue: end }, this.dynamicDuration(end - start))
+				.onUpdate(function () {
+					vm.tweeningValue = this.tweeningValue.toFixed(vm.decimals)
+				})
+				.start();
+			animate();
+		}
+	}
+});
