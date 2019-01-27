58270c6cedfcabb697a029922c881ca47c1028a7
ypnos <git@lanrules.de>
2019-01-27 23:07:52 +0100

    Fix c8738eac1b44835ceaa6e4e5ba0b4fca7d2d4f8c

--- server.py
+++ server.py
@@ -101,7 +101,7 @@ def revert(json):
 		return failure('Transaktion ungültig!')
 	if transaction.user_id != json['uid']:
 		return failure('Transaktion und Nutzer passen nicht zusammen!')
-	if transaction.date < today() or transaction.fulfilled:
+	if transaction.date < startOfDay() or transaction.fulfilled:
 		return failure('Transaktion kann nicht mehr revidiert werden!')
 	if transaction.cancelled:
 		return failure('Transaktion wurde bereits revidiert!')
